2381149cdee20bbbbff07466fb1a1aaf6f5e8be5
Joseph Quested <josephquested@gmail.com>
2017-05-31 16:55:35 +1200

    wake animation work

---
+++ Assets/Scripts/WakePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakePlayer : MonoBehaviour {
+
+	bool canWakePlayer;
+
+	void Update ()
+	{
+	  if (Input.anyKey && canWakePlayer)
+		{
+			
+			print("waking player");
+		}
+  }
+
+	public void InitCanWakePlayer ()
+	{
+		canWakePlayer = true;
+	}
+}
